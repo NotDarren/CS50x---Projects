209b859425bf562bb09dc25ac51077c59175dfbc
Darren Gan Wee Kian <darrengan0224@gmail.com>
2025-08-24 00:09:05 +0800

    Update scrabble.c

--- Problem-Sets/scrabble/scrabble.c
+++ Problem-Sets/scrabble/scrabble.c
@@ -3,18 +3,16 @@
 #include <stdio.h>
 #include <string.h>
 
-// points for each char
+
 int points[] = {1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 1, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10};
 
 int score(string s);
 
 int main(void)
 {
-    // user input for word
     string p1 = get_string("Player 1: ");
     string p2 = get_string("Player 2: ");
 
-    // calc score
     if (score(p1) < score(p2))
     {
         printf("Player 2 wins!\n");
